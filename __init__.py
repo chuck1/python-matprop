25c7f8e557049e80c8c3924f5d6f2589ac53d091
crymal <charles.rymal@nortek.com>
2016-02-15 18:29:01 -0800

    auto commit

--- __init__.py
+++ __init__.py
@@ -8,20 +8,21 @@ class Materials(object):
     def __init__(self):
         self.mat = dict()
     def add_material(self, mat):
-        self.mat[mat.name] = mat
-
+        for s in mat.names:
+            self.mat[s] = mat
+        
 class Material(object):
-    def __init__(self, name, th_cond):
-        self.name = name
+    def __init__(self, names, th_cond):
+        self.names = names
         self.props = dict()
         self.props["THERMAL_CONDUCTIVITY"] = th_cond
 
 cu = Material(
-        "copper",
+        ("copper","CU"),
         th_cond=units.unit('si_thermal_conductivity')(401.))
 
 al = Material(
-        "aluminum",
+        ("aluminum", "AL"),
         th_cond=units.unit('si_thermal_conductivity')(177.))
 
 materials = Materials()
@@ -31,9 +32,9 @@ materials.add_material(al)
 
 def PropsSI(prop_out, mat_name):
     global materials
-
+    
     mat =  materials.mat[mat_name]
-
+    
     return mat.props[prop_out]
 
 
